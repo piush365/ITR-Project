2f7ad22cd65ffe187c1216efa90145859c304410
piush365 <piushgogi@gmail.com>
2024-07-06 23:19:32 +0530

    ee linking

--- it/it-dept-script.js
+++ it/it-dept-script.js
@@ -64,6 +64,14 @@ itElements.forEach(itElement => {
   });
 });
 
+const eeElements = document.querySelectorAll("#ee");
+
+eeElements.forEach(eeElement => {
+  eeElement.addEventListener("click", () => {
+    window.location.href = "../ee/ee-dept.html";
+  });
+});
+
 const dnl = document.querySelector("#dnl-mode");
 const secBody = document.querySelector(".sec-body");
 const topHead = document.querySelector(".site-name-logo");
